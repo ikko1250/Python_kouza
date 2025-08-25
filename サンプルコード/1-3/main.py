2a8c399d6f25aeed0c9cf7767b50aad8ddbe58e4
ikko1250 <koukunpadi1250@gmail.com>
2025-08-25 03:29:45 +0000

    Refactor code structure for improved readability and maintainability

--- サンプルコード/1-3/main.py
+++ サンプルコード/1-3/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import japanize_matplotlib 
 
-df = pd.read_csv("/home/ubuntu/cur/program/Teaching/1/46鹿児島県_202507fit portal.csv")
+df = pd.read_csv("/home/ubuntu/cur/program/Teaching/1/サンプルコード/1-3/46鹿児島県_202507fit portal.csv")
 
 print("=== データの最初の5行 ===")
 print(df.head())
@@ -18,7 +18,7 @@ df_u1000 = df[df['発電出力（kW）'] <= 1000]
 
 sns.histplot(df_u1000, x='発電出力（kW）', bins=30)
 plt.title("Histogram of Power Output(<=1000kW)")
-plt.savefig("/home/ubuntu/cur/program/Teaching/1/発電出力_histogram(<=1000kW).png")
+plt.savefig("/home/ubuntu/cur/program/Teaching/1/サンプルコード/1-3/発電出力_histogram(<=1000kW).png")
 plt.clf() 
 
 vals = "太陽光"
@@ -29,4 +29,5 @@ print(df_pv[['発電出力（kW）']].describe())
 
 sns.histplot(df_pv, x='発電出力（kW）', bins=30, color="orange")
 plt.title("Histogram of Power Output(<=1000kW) for Solar Power")
-plt.savefig("/home/ubuntu/cur/program/Teaching/1/発電出力_histogram(<=1000kW)_solar.png")
+plt.savefig("/home/ubuntu/cur/program/Teaching/1/サンプルコード/1-3/発電出力_histogram(<=1000kW)_solar.png")
+plt.clf() 
